9c91a2d8f2a7bbe6505b4c1581273c750a0ec267
Felix (RuneS) <f.weissenrieder@isfas.uni-kiel.de>
2024-12-03 13:11:04 +0100

    Adjusted config

--- 2024/day03/src/part01/mullitover.py
+++ 2024/day03/src/part01/mullitover.py
@@ -1,7 +1,7 @@
 import re
 import operator
 
-with open("03/resources/data.txt", "r") as file:
+with open("aoc/2024/day03/resources/data.txt", "r") as file:
     data = file.read()
 
 
@@ -10,8 +10,4 @@ def apply_mult(data: str) -> int:
     return sum(map(lambda args: operator.mul(*map(int, args)), matches))
 
 
-def clean_do(data: str) -> str:
-    return re.sub(r"don't\(\)(.|\n)*?(do\(\)|$)", "", data)
-
-
-print(apply_mult(clean_do(data)))
+print(apply_mult(data))
